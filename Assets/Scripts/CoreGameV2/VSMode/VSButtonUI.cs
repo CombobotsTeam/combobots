62ab9cd27af1ad72adf1816de895a9b034e61386
Pierre <pierre.noel@epitech.eu>
2017-04-20 08:03:00 +0200

    Update Versus Mode

---
+++ Assets/Scripts/CoreGameV2/VSMode/VSButtonUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VSButtonUI : MonoBehaviour {
+
+    public bool bottom = false;
+    public bool top = false;
+    public int percentofsizeheight = 25;
+
+    private Vector3 Position = new Vector3(0.0f, 0.0f, 0.0f);//value of setting position
+    private VSButtonScript button;
+
+    void Start()
+    {
+        button = GetComponent<VSButtonScript>();
+
+        GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width / 4, Screen.height * percentofsizeheight / 100);
+
+        if (top)
+        {
+            Position.x = Screen.width / 4 * (3 - (int)button.Type) - Screen.width / 4;
+            Position.y = Screen.height - Screen.height / 2;
+        }
+
+        if (bottom)
+        {
+            Position.x = Screen.width / 4 * (int)button.Type - Screen.width / 2;
+            Position.y = 0 - Screen.height / 2;
+        }
+
+
+
+        GetComponent<RectTransform>().localPosition = Position;
+    }
+}
--- Assets/Scripts/CoreGameV2/VSMode/VSGameManager.cs
+++ Assets/Scripts/CoreGameV2/VSMode/VSGameManager.cs
@@ -6,7 +6,7 @@ public class VSGameManager : MonoBehaviour {
 
     // Instance to turn GameManager into Singleton
     public static VSGameManager instance = null;
-
+    public int Damage = 1;
     public int Life = 0;//life of player
 
     private int player_1_life;
@@ -14,12 +14,12 @@ public class VSGameManager : MonoBehaviour {
     private string test;
     // Contain the current combination of button pressed
     VSCombinationHandler Combinations;
+
+    CombiManager combination;
     //WaveManager WaveManager;
 
     private void Start()
     {
-        player_1_life = Life;
-        player_2_life = Life;
     }
 
     void Awake()
@@ -32,37 +32,67 @@ public class VSGameManager : MonoBehaviour {
         else if (instance != this)
             //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
             Destroy(gameObject);
-
         Combinations = GetComponent<VSCombinationHandler>();
+        combination = GetComponent<CombiManager>();
+        player_1_life = Life;
+        player_2_life = Life;
+        combination.CreateCombination();
         //WaveManager = GetComponent<WaveManager>();
     }
 
     void Update()
     {
-        //if (Combination_Player1.GetCurrentCombination().Count > 0)
-        //{
-        //    bool combinationExist = false;
-        //    if (!combinationExist)
-        //    {
-        //        Combination_Player1.Reset();
-        //    }
-        //}
+        if (Combinations.GetCurrentCombinationPlayer1().Count > 0)
+        {
+            bool PlayerHIt = false;
+            PlayerHIt = Combinations.isSameCombinationPlayer1(combination.GetCombination());
+            if (!Combinations.CompareCombinationPlayer1(combination.GetCombination()))
+            {
+                Combinations.ResetPlayer1();
+            }
+            if (PlayerHIt)
+            {
+                RemoveLife(Damage, 2);
+                combination.CreateCombination();
+                Combinations.ResetPlayer1();
+                Combinations.ResetPlayer2();
+            }
+        }
 
-        //if (Combination_Player2.GetCurrentCombination().Count > 0)
-        //{
-        //    bool combinationExist = false;
-        //    if (!combinationExist)
-        //    {
-        //        Combination_Player2.Reset();
-        //    }
-        //}
+        if (Combinations.GetCurrentCombinationPlayer2().Count > 0)
+        {
+            bool PlayerHIt = false;
+            PlayerHIt = Combinations.isSameCombinationPlayer2(combination.GetCombination());
+
+            if (!Combinations.CompareCombinationPlayer2(combination.GetCombination()))
+            {
+                Combinations.ResetPlayer2();
+            }
+            if (PlayerHIt)
+            {
+                RemoveLife(Damage, 1);
+                combination.CreateCombination();
+                Combinations.ResetPlayer1();
+                Combinations.ResetPlayer2();
+            }
+        }
+
+
     }
 
     //player's Life down(example life 3 => 2)
     public void RemoveLife(int lifeToRemove, int player)
     {
-        Life -= lifeToRemove;
-        if (Life < 0) Life = 0;
+        if (player == 1)
+        {
+            player_1_life -= lifeToRemove;
+            if (player_1_life < 0) player_1_life = 0;
+        }
+        if (player == 2)
+        {
+            player_2_life -= lifeToRemove;
+            if (player_2_life < 0) player_2_life = 0;
+        }
     }
 
     // Get the type of button pressed and update the Combination Handler
