6131b1b3b2c53f09f0395dfe106771d082fcfd61
rousse_3 <rodolphe.roussel@epitech.eu>
2017-04-30 02:11:21 +0900

    PowerUp Pawn and Boss Bishop

---
+++ Assets/Scripts/CoreGameV2/PawnPowerUp.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class PawnPowerUp : MonoBehaviour
+{
+    public int Level = 0;
+    public int Charge = 0;
+    public int ChargeMax = 100;
+    protected GameManager gm;
+
+    protected void Start()
+    {
+        gm = GameManager.instance;
+        ChargeMax = Level >= 3 ? 50 : Level >= 1 ? 75 : 100;
+    }
+
+    public void ChargePowerUp(int charge)
+    {
+        Charge += charge;
+    }
+
+    public IEnumerator desactivate(float delay)
+    {
+        float TimeUntilSummon = delay;
+
+        while (TimeUntilSummon > 0)
+        {
+            TimeUntilSummon -= Time.deltaTime;
+            yield return null;
+        }
+
+        foreach (GameObject g in gm.EnemiesOnScreen)
+        {
+            BasicEnnemy e = g.GetComponent<BasicEnnemy>();
+            e.IsMoving = true;
+            if (Level >= 4)
+                e.Speed *= 0.75f;
+        }
+    }
+
+    public void activate()
+    {
+        if (Charge < ChargeMax)
+        {
+            Debug.Log("Not full");
+            return;
+        }
+        Debug.Log("Activate");
+        Charge = 0;
+        foreach (GameObject g in gm.EnemiesOnScreen)
+        {
+            g.GetComponent<BasicEnnemy>().IsMoving = false;
+        }
+        StartCoroutine("desactivate", Level >= 2 ? 5: 3);
+    }
+}
---
+++ Assets/Scripts/CoreGameV2/Rocket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class Rocket : BasicEnnemy
+{
+    BossBlackBishop boss;
+    public float size;
+
+    public void Explode()
+    {
+        Die();
+    }
+
+    protected new void Start()
+    {
+        base.Start();
+        size = GetComponent<BoxCollider>().size.x;
+
+        boss = GameObject.Find("BlackBishop(Clone)").GetComponent<BossBlackBishop>();
+        Position = boss.addRocket(this);
+    }
+
+    public override void Die()
+    {
+        boss.RocketDeath(this);
+        base.Die();
+    }
+
+    protected override void Attack()
+    {
+        boss.RocketDeath(this);
+        base.Attack();
+    }
+}
